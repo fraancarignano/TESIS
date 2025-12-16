3e0400ff96863988d3a14daf03d4d000f738cd3f
Alan Herrera <109108802+EzeSky24@users.noreply.github.com>
2025-12-16 15:51:14 -0300

    Creacion de DTOs (create, edit, index, search)

---
+++ Backend/TESIS_OG/Controllers/InsumoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TESIS_OG.Controllers
+{
+  public class InsumoController : Controller
+  {
+    public IActionResult Index()
+    {
+      return View();
+    }
+  }
+}
